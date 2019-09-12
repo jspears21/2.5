7a169f8c6a51c8029816743037a7c1e9fadfa688
Jared Spears <jspears2@zagmail.gonzaga.edu>
2019-09-11 18:23:34 -0700

    hi

--- 2.3/ViewController.swift
+++ 2.3/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 
+    @IBAction func showMessagePressed(_ sender: UIButton) {
+        messageLabel.text = "You are Awesome!"
+        
+    }
+    
+    @IBAction func showAnotherMessage(_ sender: UIButton) {
+        messageLabel.text = "You are Great!"
+    }
 
+    
 }
 
